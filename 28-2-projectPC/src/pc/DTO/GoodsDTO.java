0015ba393e5549dd653b6f0b82ab7b56b6491527
songyoobin <funny5311@gmail.com>
2018-06-29 15:52:37 +0900

    [송유빈] 1.projgect_pc_SQL.txt / COLLATE euckr로 수정 2.GoodsDTO.java 추가 3.GoodsDAO.java 추가

--- 28-2-projectPC/src/pc/DTO/GoodsDTO.java
+++ 28-2-projectPC/src/pc/DTO/GoodsDTO.java
@@ -1,5 +1,28 @@
 package pc.DTO;
 
 public class GoodsDTO {
-
+	public int goods_code;
+	public String goods_name; 
+	public int goods_price;
+	
+	
+	public int getGoods_code() {
+		return goods_code;
+	}
+	public void setGoods_code(int goods_code) {
+		this.goods_code = goods_code;
+	}
+	public String getGoods_name() {
+		return goods_name;
+	}
+	public void setGoods_name(String goods_name) {
+		this.goods_name = goods_name;
+	}
+	public int getGoods_price() {
+		return goods_price;
+	}
+	public void setGoods_price(int goods_price) {
+		this.goods_price = goods_price;
+	}
+	
 }
--- 28-2-projectPC/src/pc/goodsDAO/GoodsDAO.java
+++ 28-2-projectPC/src/pc/goodsDAO/GoodsDAO.java
@@ -1,5 +1,26 @@
 package pc.goodsDAO;
 
+import pc.DTO.GoodsDTO;
+import pc.driver.Driver;
+import java.sql.*;
+
 public class GoodsDAO {
+	Connection connection = null;
+	PreparedStatement pstmt = null;
+
+	// 01 상품 입고 insertGoods
+	public void insertGoods(GoodsDTO goods) throws ClassNotFoundException, SQLException {
+		System.out.println("01 insertGoods");
+		Driver db = new Driver();
+		connection = db.driverConnection();
+		System.out.println("connection : " + connection);
+		pstmt = connection.prepareStatement("INSERT INTO pc_goods(goods_name,goods_price) VALUES(?,?)");
+		pstmt.setString(2, goods.getGoods_name());
+		pstmt.setInt(3, goods.getGoods_price());
+		pstmt.executeUpdate();
+
+		pstmt.close();
+		connection.close();
+	}
 
 }
