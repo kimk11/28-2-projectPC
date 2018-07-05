dcbc5c4299625abc7e7b7c9e2b81d00cfebf1349
songyoobin <funny5311@gmail.com>
2018-07-05 10:46:37 +0900

    [송유빈] 1.OrderDAO.java | orderHistory 메서드 추가

--- 28-2-projectPC/src/pc/orderDAO/OrderDAO.java
+++ 28-2-projectPC/src/pc/orderDAO/OrderDAO.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 import pc.driver.Driver;
+import pc.DTO.GoodsDTO;
 import pc.DTO.OrderDTO;
 
 public class OrderDAO {
@@ -16,4 +17,32 @@ public class OrderDAO {
 //상품 주문 상품 >> 상품 리스트에서 해당 물풀 클릭시 db에저장
 	
 //주문 내역 >> db 리스트 
+	public ArrayList<OrderDTO> orderHistory() throws ClassNotFoundException, SQLException {
+		System.out.println("01 orderHistory <OrederDAO>");
+		ArrayList<OrderDTO> arOrderList = new ArrayList<>();
+		Driver db = new Driver();
+		connection = db.driverConnection();
+		System.out.println("connection : " + connection);
+		
+		String sql = "SELECT order_code,user_id,goods_code,seat_no,goods_price FROM pc_order";
+		preparedstatement = connection.prepareStatement(sql);
+		
+		resultset = preparedstatement.executeQuery();
+		
+		while(resultset.next()) {
+			OrderDTO orderDto = new OrderDTO();
+			orderDto.setOrderCode(resultset.getInt(1));
+			orderDto.setUserId(resultset.getString(2));
+			orderDto.setGoodsCode(resultset.getInt(3));
+			orderDto.setSeatNo(resultset.getInt(4));
+			orderDto.setGoodsPrice(resultset.getInt(5));
+			arOrderList.add(orderDto);
+			
+		}
+		
+		resultset.close();
+		preparedstatement.close();
+		connection.close();
+		return arOrderList;
+	}
 }
